0949978690ede571835ce4577619a3cbf65fa4c2
陳建安 <a19880419@gmail.com>
2020-10-22 11:00:41 +0800

    first edition of crnn model

---
+++ crnn/dataset.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+import random
+import cv2
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data import sampler
+import torchvision.transforms as transforms
+import lmdb
+import sys
+import numpy as np
+
+
+class lmdbDataset(Dataset):
+
+    def __init__(self, root=None):
+        self.env = lmdb.open(
+            root,
+            max_readers=1,
+            readonly=True,
+            lock=False,
+            readahead=False,
+            meminit=False)
+
+        if not self.env:
+            print('cannot creat lmdb from %s' % (root))
+            sys.exit(0)
+
+        with self.env.begin(write=False) as txn:
+            nSamples = int(txn.get('num-samples'.encode()).decode())
+            self.nSamples = nSamples
+
+    def __len__(self):
+        return self.nSamples
+
+    def __getitem__(self, index):
+        assert index <= len(self), 'index range error'
+        index += 1
+        with self.env.begin(write=False) as txn:
+            input_key = 'input-%05d' % index
+            inputbuf = txn.get(input_key.encode())
+            input_data = np.frombuffer(inputbuf, dtype=np.float64)
+
+            label_key = 'label-%05d' % index
+            label = txn.get(label_key.encode()).decode()
+
+        return (input_data, label)
+
+# resize each image
+class resizeNormalize:
+
+    def __init__(self, size, interpolation=cv2.INTER_LINEAR):
+        self.size = size
+        self.interpolation = interpolation
+        self.toTensor = transforms.ToTensor()
+
+    def __call__(self, input_data):
+        img = cv2.resize(input_data, self.size, interpolation=self.interpolation)
+        img = self.toTensor(img)
+        return img
+
+######### 可以改寫的 #########
+class randomSequentialSampler(sampler.Sampler):
+
+    def __init__(self, data_source, batch_size):
+        self.num_samples = len(data_source)
+        self.batch_size = batch_size
+
+    def __iter__(self):
+        n_batch = self.num_samples // self.batch_size
+        tail = self.num_samples % self.batch_size
+        index = torch.LongTensor(self.num_samples).fill_(0)
+        for i in range(n_batch):
+            random_start = random.randint(0, self.num_samples - self.batch_size)
+            batch_index = random_start + torch.range(0, self.batch_size - 1)
+            index[i * self.batch_size:(i + 1) * self.batch_size] = batch_index
+        # deal with tail
+        if tail:
+            random_start = random.randint(0, self.num_samples - self.batch_size)
+            tail_index = random_start + torch.range(0, tail - 1)
+            index[(i + 1) * self.batch_size:] = tail_index
+
+        return iter(index)
+
+    def __len__(self):
+        return self.num_samples
+###########################
+
+class alignCollate:
+
+    def __init__(self, H=50, W=200):
+        self.H = H
+        self.W = W
+
+    def __call__(self, batch):
+        inputs, labels = zip(*batch)
+        H = self.H
+        W = self.W
+        transform = resizeNormalize((W, H))
+        inputs = [transform(input_data) for input_data in inputs]
+        inputs = torch.cat([t.unsqueeze(0) for t in inputs], 0)
+
+        return inputs, labels
---
+++ crnn/models/crnn.py
@@ -0,0 +1,76 @@
+import torch.nn as nn
+
+
+class BidirectionalLSTM(nn.Module):
+
+    def __init__(self, nIn, nHidden, nOut):
+        super(BidirectionalLSTM, self).__init__()
+
+        self.rnn = nn.LSTM(nIn, nHidden, bidirectional=True)
+        self.embedding = nn.Linear(nHidden * 2, nOut)
+
+    def forward(self, input):
+        recurrent, _ = self.rnn(input)
+        T, b, h = recurrent.size()
+        t_rec = recurrent.view(T * b, h)
+
+        output = self.embedding(t_rec)  # [T * b, nOut]
+        output = output.view(T, b, -1)
+
+        return output
+
+
+class CRNN(nn.Module):
+
+    def __init__(self, nc, nclass, rnn_node, n_rnn=2, leakyRelu=False):
+        super(CRNN, self).__init__()
+        
+        nm = [32, 32, 32 ,32, 32, 32]
+        ks = [(3, 7), (3, 7), (3, 7), (3, 7), (3, 7), (3, 7)]
+        ss = [1, 1, 1, 1, 1, 1]
+        ps = [0, 0, 0, 0, 0, 0]
+        cnn = nn.Sequential()
+
+        def convRelu(i, batchNormalization=False):
+            nIn = nc if i == 0 else nm[i - 1]
+            nOut = nm[i]
+            cnn.add_module('conv{0}'.format(i),
+                           nn.Conv2d(in_channels=nIn,
+                           out_channels=nOut,
+                           kernel_size=ks[i],
+                           stride=ss[i],
+                           padding=ps[i]))
+            if batchNormalization:
+                cnn.add_module('batchnorm{0}'.format(i), nn.BatchNorm2d(nOut))
+            if leakyRelu:
+                cnn.add_module('relu{0}'.format(i),
+                               nn.LeakyReLU(0.2, inplace=True))
+            else:
+                cnn.add_module('relu{0}'.format(i), nn.ReLU(True))
+
+        convRelu(0)
+        convRelu(1)
+        cnn.add_module('pooling{0}'.format(1), nn.MaxPool2d(2, 1))
+        convRelu(2)
+        convRelu(3)
+        cnn.add_module('pooling{0}'.format(2), nn.MaxPool2d(2, 1))
+        convRelu(4)
+        convRelu(5)
+        cnn.add_module('pooling{0}'.format(3), nn.MaxPool2d(2, 1))
+
+        self.cnn = cnn
+        self.rnn = nn.Sequential(
+            BidirectionalLSTM(nm[-1], rnn_node, rnn_node),
+            BidirectionalLSTM(rnn_node, rnn_node, nclass))
+
+    def forward(self, input):
+        # conv features
+        conv = self.cnn(input)
+        b, c, h, w = conv.size()
+        conv = conv.reshape(b,c,h*w)
+        conv = conv.permute(2, 0, 1)  # [w, b, c]
+
+        # rnn features
+        output = self.rnn(conv)
+
+        return output
